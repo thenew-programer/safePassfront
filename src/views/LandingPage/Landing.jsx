0ab55a476df80a2221b7fd1212280dad370982dd
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-06-10 14:45:28 +0100

    errors

--- src/views/LandingPage/Landing.jsx
+++ src/views/LandingPage/Landing.jsx
@@ -3,11 +3,26 @@ import React from "react"
 
 const Landing = () => {
 	return (
-		<>
+		<div className="landing-container">
+			<div className="landing-top">
+				<nav>
+					<div className="logo">
+						<h1><span>Safe</span>pass</h1>
+					</div>
+					<ul className="nav-list">
+						<li><a href=""></a></li>
+						<li><a href=""></a></li>
+						<li><a href=""></a></li>
+						<li><a href=""></a></li>
+						<li><a href=""></a></li>
+						<li><a href=""></a></li>
+					</ul>
+				</nav>
+			</div>
 
-		</>
+		</div >
 	)
 }
 
-export default Landing
+export default Landing;
 
